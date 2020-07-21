159a62165f806e5a9a0e5081f68c3e85cde99866
Justine <pignet.justine@gmail.com>
2020-07-21 14:22:11 +0200

    tests

--- src/Entity/BlackJack.php
+++ src/Entity/BlackJack.php
@@ -2,7 +2,12 @@
 
 namespace App\Entity;
 
+use phpDocumentor\Reflection\Types\String_;
+
 class BlackJack
 {
+    public function play(array $cardDecks) : String
+    {
 
+    }
 }
--- src/Test/BlackJackTest.php
+++ src/Test/BlackJackTest.php
@@ -3,8 +3,25 @@
 namespace App\Test;
 
 use PHPUnit\Framework\TestCase;
+use App\Entity\BlackJack;
 
 class BlackJackTest extends TestCase
 {
+    public function bankWins() {
+        $blackJack = new BlackJack();
 
+        $this->assertEquals("bank", $blackJack->play());
+    }
+
+    public function playerWins() {
+        $blackJack = new BlackJack();
+
+        $this->assertEquals("player", $blackJack->play());
+    }
+
+    public function par() {
+        $blackJack = new BlackJack();
+
+        $this->assertEquals("par", $blackJack->play());
+    }
 }
